3f6c7a73a27cb931a28089435fb461a0ef3ba5e6
Dario Alvarez <dariopy@gmail.com>
2011-05-07 19:49:42 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simplemind::Application.config.secret_token = '289ebb7fc5511893066870d8bf3b5886b8df2e0d25701625780e7ecf8e5825acfcfdabe6f1325ecd2ab8ff49eecc52a4e2b9660c1bd5eeef8200bae6256f6b56'
